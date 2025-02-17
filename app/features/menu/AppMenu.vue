ffc56400737edcb53d45dfb788414850ae5c5348
Peterko <peter.ko@mail.ru>
2025-02-17 16:03:22 +0300

    feat: вынес циклы отдельно

--- app/features/menu/AppMenu.vue
+++ app/features/menu/AppMenu.vue
@@ -2,39 +2,9 @@
   import { SvgIcon, SvgLogo } from '~/shared/ui';
   import { PopoverMenu } from './ui';
 
+  import { socialLinks, supportLinks } from './model/const';
+
   const isShowMenu = ref(false);
-
-  const socialLinks = [
-    {
-      url: 'https://vk.com/ttg.club',
-      icon: 'vk',
-    },
-    {
-      url: 'https://discord.gg/JqFKMKRtxv',
-      icon: 'discord',
-    },
-    {
-      url: 'https://t.me/ttgclubnews',
-      icon: 'telegram',
-    },
-    {
-      url: 'https://www.youtube.com/channel/UCpFse6-P2IBXYfkesAxZbfA',
-      icon: 'youtube',
-    },
-  ];
-
-  const supportLinks = [
-    {
-      url: 'https://boosty.to/dnd5club',
-      icon: 'boosty',
-      label: 'TTG Club',
-    },
-    {
-      url: 'https://boosty.to/dnd5eclub',
-      icon: 'boosty',
-      label: 'Magistrus',
-    },
-  ];
 </script>
 
 <template>
