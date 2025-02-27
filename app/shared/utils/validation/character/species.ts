a2669607843645e8d60cd3ef21c48ea955759bf2
Nikita Charushin <45195424+svifty7@users.noreply.github.com>
2025-02-27 14:13:14 +0300

    fix: правила валидации в редакторах (#21)
    * fix: [TTG-75] проверка url в редакторе вида
    
    * fix: [TTG-76] проверка url в редакторе заклинания
    
    * fix: [TTG-72] расширен список символов в названиях

--- app/shared/utils/validation/character/species.ts
+++ app/shared/utils/validation/character/species.ts
@@ -6,22 +6,23 @@ export const ruleUrl = (): Rule => ({
   required: true,
   trigger: ['change', 'blur'],
   type: 'string',
-  validator: async (rule: Rule, value: string) => {
-    baseStringCheck(value, 3);
+  validator: (rule: Rule, value: string) =>
+    new Promise((resolve, reject) => {
+      baseStringCheck(value, 3);
 
-    await $fetch(`/api/v2/species/${value}`, {
-      method: 'head',
-      retry: false,
-      onRequestError: () => {
-        throw new Error('Неизвестная ошибка');
-      },
-      onResponseError: (response) => {
-        throw new Error(
-          response.response.status === StatusCodes.NOT_FOUND
-            ? 'Такой вид уже существует'
-            : 'Неизвестная ошибка сервера',
-        );
-      },
-    });
-  },
+      $fetch(`/api/v2/species/${value}`, {
+        method: 'head',
+        retry: false,
+        onRequestError: () => {
+          reject('Неизвестная ошибка');
+        },
+        onResponseError: (response) => {
+          if (response.response.status === StatusCodes.NOT_FOUND) {
+            return resolve();
+          }
+
+          return reject('Неизвестная ошибка');
+        },
+      }).then(() => reject('Такой вид уже существует'));
+    }),
 });
