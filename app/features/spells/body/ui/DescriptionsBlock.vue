28b8b15574cd444b199b00b6b21d46d5da5d74e9
svifty7 <svifty7@gmail.com>
2025-03-14 23:49:20 +0300

    feat: [TTG-56] первая попытка в маркап

--- app/features/spells/body/ui/DescriptionsBlock.vue
+++ app/features/spells/body/ui/DescriptionsBlock.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
   import type { SpellDetailResponse } from '~/shared/types';
+  import { MarkupRender } from '~ui/markup';
 
-  defineProps<Pick<SpellDetailResponse, 'description' | 'level' | 'upper'>>();
+  const { description } =
+    defineProps<Pick<SpellDetailResponse, 'description' | 'level' | 'upper'>>();
+
+  const descriptionEntries = computed(() => description.split('\n\n'));
 </script>
 
 <template>
-  <div :style="{ whiteSpace: 'pre-wrap' }">
-    {{ description }}
-  </div>
+  <MarkupRender :entries="descriptionEntries" />
 
   <div v-if="upper">
     <strong v-if="!level">Улучшение заговора. </strong>
---
+++ app/shared/ui/markup/MarkupRender.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import { markupService } from './renderer';
+
+  const { entries } = defineProps<{
+    entries: string | Array<string>;
+  }>();
+
+  const renderedEntries = markupService.render(entries);
+</script>
+
+<template>
+  <ClientOnly>
+    <component :is="renderedEntries" />
+  </ClientOnly>
+</template>
