08d0d96d203904b7d49428469844061ac6e34866
svifty7 <svifty7@gmail.com>
2025-03-15 21:34:06 +0300

    feat: [TTG-56] маркеры в json

--- app/shared/ui/markup/renderer.ts
+++ app/shared/ui/markup/renderer.ts
@@ -1,250 +1,331 @@
-import type { VNode } from 'vue';
-import { h } from 'vue';
-import { toNumber } from 'lodash-es';
-
-const LEADING_CHARACTER = '@';
+// Наивысшая допустимая глубина вложенности.
 const MAX_DEPTH = 7;
+// Максимальный размер строки.
+const MAX_STRING_LENGTH = 65536;
+// Первый символ маркера
+const LEADING_CHARACTER = '@';
 
-export class MarkupRenderer {
-  public render(entries: string | Array<string>): VNode {
-    if (!entries) return h('div', 'Произошла какая-то ошибка...');
+// Разрешенные маркеры и их алиасы.
+export const enum MarkerType {
+  Bold = 'bold',
+  Italic = 'italic',
+  Underline = 'underline',
+  Strikethrough = 'strikethrough',
+  Link = 'link',
+  Br = 'br',
+}
 
-    if (Array.isArray(entries)) {
-      return h(
-        'div',
-        entries.map((entry) => this.recursiveRender(entry.trim())),
-      );
+// Разрешенные алиасы для маркеров
+const MARKERS = new Map<string, MarkerType>([
+  ['bold', MarkerType.Bold],
+  ['b', MarkerType.Bold],
+  ['italic', MarkerType.Italic],
+  ['i', MarkerType.Italic],
+  ['underline', MarkerType.Underline],
+  ['u', MarkerType.Underline],
+  ['strikethrough', MarkerType.Strikethrough],
+  ['s', MarkerType.Strikethrough],
+  ['link', MarkerType.Link],
+  ['br', MarkerType.Br],
+]);
+
+// Типы данных для параметров атрибутов.
+export type ParamValue = string | number | boolean | null;
+
+// Тип для параметров маркера.
+export type MarkerAttributes = Record<string, ParamValue>;
+
+export interface TextMarkerNode {
+  type: 'text';
+  text: string;
+}
+
+export interface EmptyMarkerNode {
+  type: MarkerType;
+}
+
+export interface CustomizedMarkerNode extends EmptyMarkerNode {
+  attrs?: MarkerAttributes;
+  content: Array<MarkerNode>;
+}
+
+// Типы узлов TipTap.
+export type MarkerNode =
+  | TextMarkerNode
+  | EmptyMarkerNode
+  | CustomizedMarkerNode;
+
+export function parseString(text: string): MarkerNode[] {
+  if (text.length > MAX_STRING_LENGTH) {
+    throw new Error('[Markup] String is too long');
+  }
+
+  const depth = 0;
+
+  return recursiveParse(text, depth);
+}
+
+function recursiveParse(text: string, depth: number): MarkerNode[] {
+  if (depth > MAX_DEPTH) {
+    throw new Error('[Markup] Maximum nesting depth exceeded');
+  }
+
+  const tagSplit = splitByMarkers(text);
+  const len = tagSplit.length;
+  const result: Array<MarkerNode> = [];
+
+  for (let i = 0; i < len; ++i) {
+    const str = tagSplit[i];
+
+    if (!str) {
+      continue;
     }
 
-    return h('div', this.recursiveRender(entries.trim()));
-  }
+    if (str.startsWith(`{${LEADING_CHARACTER}`)) {
+      const [marker, text] = splitFirstSpace(str.slice(1, -1));
 
-  private recursiveRender(
-    entry: string,
-    depth: number = 0,
-  ): Array<VNode | string> {
-    if (depth > MAX_DEPTH) return [entry];
-
-    const parts = this.splitByMarkers(entry);
-    const result: Array<VNode | string> = [];
-
-    let lastText = '';
-
-    for (const part of parts) {
-      if (part.startsWith(`{${LEADING_CHARACTER}`) && part.endsWith('}')) {
-        if (lastText) {
-          result.push(lastText);
-          lastText = '';
-        }
-
-        const inner = part.slice(1, -1);
-        const [marker, textWithParams] = this.splitFirstSpace(inner);
-        const [text, ...params] = this.splitByPipeBase(textWithParams);
-
-        result.push(this.renderVNode(marker, text || '', params, depth));
-      } else {
-        lastText += part;
-      }
-    }
-
-    if (lastText) result.push(lastText);
-
-    if (depth === 0 && !parts.some((part) => part.includes('{@heading'))) {
-      return [h('p', result)];
-    }
-
-    return result;
-  }
-
-  private renderVNode(
-    marker: string,
-    text: string,
-    params: Array<string>,
-    depth: number,
-  ): VNode | string {
-    if (depth > MAX_DEPTH) return h('span', text);
-
-    switch (marker) {
-      case '@b':
-      case '@bold':
-        return this.renderBold(text, depth);
-      case '@i':
-      case '@italic':
-        return this.renderItalic(text, depth);
-      case '@s':
-      case '@strike':
-      case '@strikethrough':
-        return this.renderStrike(text, depth);
-      case '@u':
-      case '@underline':
-        return this.renderUnderline(text, depth);
-      case '@link':
-        return this.renderLink(text, depth);
-      case '@heading':
-        return this.renderHeading(text, params);
-      default:
-        return h('span', text);
+      result.push(convertMarker(marker, text, depth));
+    } else {
+      result.push(convertText(str));
     }
   }
 
-  private renderHeading = (text: string, params: Array<string>): VNode => {
-    const [level] = params;
+  return result;
+}
 
-    const validLevel =
-      level && /^[1-4]$/.test(level.trim()) ? level.trim() : '2';
+function convertMarker(
+  marker: MarkerType,
+  textWithParams: string,
+  depth: number,
+): MarkerNode {
+  switch (marker) {
+    case MarkerType.Bold:
+    case MarkerType.Italic:
+    case MarkerType.Underline:
+    case MarkerType.Strikethrough:
+    case MarkerType.Link:
+      return convertMarkerSimple(marker, textWithParams, depth);
+    case MarkerType.Br:
+      return convertBr();
+    default:
+      throw new Error(`[Markup] Unknown marker: ${marker}`);
+  }
+}
 
-    return h(`h${toNumber(validLevel) + 2}`, text);
-  };
-
-  private renderBold(text: string, depth: number) {
-    return h('b', this.recursiveRender(text || '', depth + 1));
+function convertText(text: string | undefined): TextMarkerNode {
+  if (!text) {
+    throw new Error(`[Markup] Text marker must have text`);
   }
 
-  private renderItalic(text: string, depth: number) {
-    return h('i', this.recursiveRender(text || '', depth + 1));
-  }
-
-  private renderStrike(text: string, depth: number) {
-    return h('s', this.recursiveRender(text || '', depth + 1));
-  }
-
-  private renderUnderline(text: string, depth: number) {
-    return h('u', this.recursiveRender(text || '', depth + 1));
-  }
-
-  private renderLink(text: string, depth: number) {
-    const [label, url] = this.splitByPipeBase(text);
-
-    return h(
-      'a',
-      { href: url || '#', target: '_blank', rel: 'noopener noreferrer' },
-      this.recursiveRender(label || '', depth + 1),
-    );
-  }
-
-  private splitFirstSpace = (string: string): [string, string] => {
-    const firstIndex = string.indexOf(' ');
-
-    return firstIndex !== -1
-      ? [string.substring(0, firstIndex), string.substring(firstIndex + 1)]
-      : [string, ''];
-  };
-
-  private splitByMarkers = (string: string): string[] => {
-    let tagDepth = 0;
-    let char;
-    let char2;
-
-    const out: string[] = [];
-
-    let curStr = '';
-    let isLastOpen = false;
-
-    const len = string.length;
-
-    for (let i = 0; i < len; ++i) {
-      char = string[i];
-      char2 = string[i + 1] || '';
-
-      switch (char) {
-        case '{':
-          isLastOpen = true;
-
-          if (char2 === LEADING_CHARACTER) {
-            if (tagDepth++ > 0) {
-              curStr += '{';
-            } else {
-              out.push(curStr.replace(/<VE_LEAD>/g, LEADING_CHARACTER));
-              curStr = `{${LEADING_CHARACTER}`;
-              ++i;
-            }
-          } else {
-            curStr += '{';
-          }
-
-          break;
-
-        case '}':
-          isLastOpen = false;
-          curStr += '}';
-
-          if (tagDepth !== 0 && --tagDepth === 0) {
-            out.push(curStr.replace(/<VE_LEAD>/g, LEADING_CHARACTER));
-            curStr = '';
-          }
-
-          break;
-
-        case LEADING_CHARACTER: {
-          if (!isLastOpen) {
-            curStr += '<VE_LEAD>';
-          } else {
-            curStr += LEADING_CHARACTER;
-          }
-
-          break;
-        }
-
-        default:
-          isLastOpen = false;
-          curStr += char;
-
-          break;
-      }
-    }
-
-    if (curStr) {
-      out.push(curStr.replace(/<VE_LEAD>/g, LEADING_CHARACTER));
-    }
-
-    return out;
-  };
-
-  private splitByPipeBase = (string: string): string[] => {
-    let tagDepth = 0;
-
-    const out: string[] = [];
-
-    let curStr = '';
-
-    for (let i = 0; i < string.length; ++i) {
-      const char = string[i];
-      const char2 = string[i + 1] || '';
-
-      switch (char) {
-        case '{':
-          if (char2 === LEADING_CHARACTER) tagDepth++;
-          curStr += '{';
-
-          break;
-
-        case '}':
-          if (tagDepth) tagDepth--;
-          curStr += '}';
-
-          break;
-
-        case '|':
-          if (tagDepth) {
-            curStr += '|';
-          } else {
-            out.push(curStr.trim());
-            curStr = '';
-          }
-
-          break;
-
-        default:
-          curStr += char;
-
-          break;
-      }
-    }
-
-    if (curStr) out.push(curStr.trim());
-
-    return out;
+  return {
+    type: 'text',
+    text,
   };
 }
 
-export const markupService = new MarkupRenderer();
+function convertMarkerSimple(
+  marker: MarkerType,
+  textWithParams: string,
+  depth: number,
+): CustomizedMarkerNode {
+  const { text, params } = splitByPipeBase(textWithParams);
+
+  if (!text) {
+    throw new Error(
+      `[Markup] ${marker.charAt(0).toUpperCase() + marker.slice(1)} marker must have text`,
+    );
+  }
+
+  return {
+    type: marker,
+    attrs: splitAttrs(params),
+    content: recursiveParse(text, depth),
+  };
+}
+
+function convertBr(): EmptyMarkerNode {
+  return {
+    type: MarkerType.Br,
+  };
+}
+
+function splitByMarkers(string: string): string[] {
+  let tagDepth = 0;
+  let char;
+
+  let char2;
+
+  const out = [];
+
+  let curStr = '';
+  let isLastOpen = false;
+
+  const len = string.length;
+
+  for (let i = 0; i < len; ++i) {
+    char = string[i];
+    char2 = string[i + 1];
+
+    switch (char) {
+      case '{':
+        isLastOpen = true;
+
+        if (char2 === LEADING_CHARACTER) {
+          if (tagDepth++ > 0) {
+            curStr += '{';
+          } else {
+            out.push(curStr.replace(/<LEAD_CHAR>/g, LEADING_CHARACTER));
+            curStr = `{${LEADING_CHARACTER}`;
+            ++i;
+          }
+        } else {
+          curStr += '{';
+        }
+
+        break;
+
+      case '}':
+        isLastOpen = false;
+        curStr += '}';
+
+        if (tagDepth !== 0 && --tagDepth === 0) {
+          out.push(curStr.replace(/<LEAD_CHAR>/g, LEADING_CHARACTER));
+          curStr = '';
+        }
+
+        break;
+
+      case LEADING_CHARACTER: {
+        if (!isLastOpen) {
+          curStr += '<LEAD_CHAR>';
+        } else {
+          curStr += LEADING_CHARACTER;
+        }
+
+        break;
+      }
+
+      default:
+        isLastOpen = false;
+        curStr += char;
+
+        break;
+    }
+  }
+
+  if (curStr) {
+    out.push(curStr.replace(/<LEAD_CHAR>/g, LEADING_CHARACTER));
+  }
+
+  return out;
+}
+
+function splitByPipeBase(string: string): { text?: string; params: string[] } {
+  let tagDepth = 0;
+  let curStr = '';
+
+  const len = string.length;
+
+  const params: string[] = [];
+
+  let text: string | undefined;
+
+  for (let i = 0; i < len; ++i) {
+    const char = string[i];
+    const char2 = string[i + 1];
+
+    switch (char) {
+      case '{':
+        if (char2 === LEADING_CHARACTER) tagDepth++;
+        curStr += '{';
+
+        break;
+
+      case '}':
+        if (tagDepth) tagDepth--;
+        curStr += '}';
+
+        break;
+
+      case '|': {
+        if (tagDepth) {
+          curStr += '|';
+        } else {
+          if (!text) {
+            text = curStr;
+          } else {
+            params.push(curStr);
+          }
+
+          curStr = '';
+        }
+
+        break;
+      }
+
+      default: {
+        curStr += char;
+
+        break;
+      }
+    }
+  }
+
+  if (curStr) {
+    if (!text) {
+      text = curStr;
+    } else {
+      params.push(curStr);
+    }
+  }
+
+  return { text: text?.trim(), params: params.map((param) => param.trim()) };
+}
+
+function splitFirstSpace(string: string): [MarkerType, string] {
+  const index = string.indexOf(' ');
+  const rawMarker = index < 0 ? string : string.substring(0, index);
+  const marker = MARKERS.get(rawMarker.replace(/^@/, ''));
+
+  if (!marker) {
+    throw new Error(`[Markup] Unknown marker: ${rawMarker}`);
+  }
+
+  return [marker, index < 0 ? '' : string.substring(index + 1)];
+}
+
+function splitAttrs(params: string[]): MarkerAttributes {
+  const attrs: MarkerAttributes = {};
+
+  for (const param of params) {
+    const index = param.indexOf(':');
+
+    if (index < 0) {
+      throw new Error(`[Markup] Attribute must have value: ${param}`);
+    }
+
+    const name = param.substring(0, index);
+
+    if (!name) {
+      throw new Error(`[Markup] Attribute name is required: ${param}`);
+    }
+
+    const value = param.substring(index + 1);
+
+    if (!value) {
+      throw new Error(`[Markup] Attribute value is required: ${name}`);
+    }
+
+    attrs[name] = parseAttrValue(value);
+  }
+
+  return attrs;
+}
+
+function parseAttrValue(attrValue: string): ParamValue {
+  try {
+    return JSON.parse(attrValue);
+  } catch (err) {
+    return attrValue;
+  }
+}
