d565feaf81cbf8139b1fe0b60e894999c9777d76
svifty7 <svifty7@gmail.com>
2025-04-28 21:04:08 +0300

    fix: исправление иконок

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -78,8 +78,8 @@ export default defineNuxtConfig({
         },
         {
           rel: 'icon',
-          type: 'image/x-icon',
-          href: '/favicon.ico',
+          type: 'image/svg+xml',
+          href: '/favicons/favicon.svg',
         },
         {
           rel: 'apple-touch-icon',
