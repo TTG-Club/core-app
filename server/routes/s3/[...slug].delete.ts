14cfd7d7da3eeca5cd00e12a2c60db60b29562a0
svifty7 <svifty7@gmail.com>
2025-08-08 17:44:08 +0300

    fix: разрешение админам удалять файлы с s3 от любого пользователя

--- server/routes/s3/[...slug].delete.ts
+++ server/routes/s3/[...slug].delete.ts
@@ -1,20 +1,45 @@
 import { StatusCodes } from 'http-status-codes';
 
 import { S3Service } from '~~/server/services';
+import type { H3Event } from 'h3';
+import { Role } from '~/shared/types';
 
 export default defineEventHandler((event) => {
-  const { username } = getUserFromToken(event);
   const slug = getRouterParam(event, 'slug');
 
   if (!slug) {
     throw createError(getErrorResponse(StatusCodes.BAD_REQUEST));
   }
 
-  const [, user] = slug.split('/');
+  const [, username] = slug.split('/');
 
-  if (!user || getSlug(username) !== user) {
+  try {
+    checkUserAccess(event, username);
+  } catch (e) {
     throw createError(getErrorResponse(StatusCodes.FORBIDDEN));
   }
 
   return S3Service.delete(slug);
 });
+
+function checkUserAccess(event: H3Event, username: string) {
+  const { username: usernameFromToken, roles } = getUserFromToken(event);
+
+  if (!usernameFromToken) {
+    throw createError(getErrorResponse(StatusCodes.UNAUTHORIZED));
+  }
+
+  if (usernameFromToken === username) {
+    return;
+  }
+
+  if (
+    !roles.some(
+      (role) => role.name === Role.ADMIN || role.name === Role.MODERATOR,
+    )
+  ) {
+    return;
+  }
+
+  throw createError(getErrorResponse(StatusCodes.FORBIDDEN));
+}
