2ce2cf89ce86c106efb75fcbf8873918d38cfffa
svifty7 <svifty7@gmail.com>
2025-08-18 19:22:20 +0300

    feat: изменил способ заполнения сопротивления, уязвимостей и иммунитетов

---
+++ app/features/bestiary/editor/ui/defenses/ui/CreatureResistances.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+  import { SelectDamageType } from '~ui/select';
+  import type { CreatureDefense } from '~bestiary/types';
+
+  const resistances = defineModel<CreatureDefense>({
+    required: true,
+  });
+</script>
+
+<template>
+  <UForm
+    :state="resistances"
+    attach
+    class="col-span-12 flex flex-col gap-4"
+  >
+    <UFormField
+      label="Сопротивление к типам урона"
+      name="values"
+    >
+      <SelectDamageType
+        v-model="resistances.values"
+        multiple
+      />
+    </UFormField>
+
+    <UFormField name="text">
+      <UInput
+        v-model="resistances.text"
+        placeholder="Введи отображаемый текст сопротивления"
+      />
+    </UFormField>
+  </UForm>
+</template>
