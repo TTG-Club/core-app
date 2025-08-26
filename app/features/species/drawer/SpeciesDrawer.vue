e4c34a6810582d2977e03ec1f6a21c5d22cd9c42
Peterko <peter.ko@mail.ru>
2025-08-26 11:33:13 +0300

    feat: добавление информация в предпросмотр видов

--- app/features/species/drawer/SpeciesDrawer.vue
+++ app/features/species/drawer/SpeciesDrawer.vue
@@ -39,6 +39,33 @@
     copy-title
     @close="$emit('close')"
   >
+    <div
+      v-if="detail"
+      class="mb-4 overflow-hidden rounded-lg border border-default bg-muted py-1.5"
+    >
+      <div class="flex w-full min-w-full gap-0 px-4 py-1.5">
+        <span class="min-w-20 text-sm font-medium text-highlighted">Тип:</span>
+
+        <span>{{ detail.properties.type }}</span>
+      </div>
+
+      <div class="flex w-full min-w-full gap-0 px-4 py-1.5">
+        <span class="min-w-20 text-sm font-medium text-highlighted"
+          >Размер:</span
+        >
+
+        <span>{{ detail.properties.size }}</span>
+      </div>
+
+      <div class="flex w-full min-w-full gap-0 px-4 py-1.5">
+        <span class="min-w-20 text-sm font-medium text-highlighted"
+          >Скорость:</span
+        >
+
+        <span>{{ detail.properties.speed }}</span>
+      </div>
+    </div>
+
     <SpeciesBody
       v-if="detail"
       :species="detail"
--- app/features/species/preview/SpeciesPreview.vue
+++ app/features/species/preview/SpeciesPreview.vue
@@ -23,6 +23,33 @@
     :is-error
     width="100%"
   >
+    <div
+      v-if="species"
+      class="mb-4 overflow-hidden rounded-lg border border-default bg-muted py-1.5"
+    >
+      <div class="flex w-full min-w-full gap-0 px-4 py-1.5">
+        <span class="min-w-20 text-sm font-medium text-highlighted">Тип:</span>
+
+        <span>{{ species.properties.type }}</span>
+      </div>
+
+      <div class="flex w-full min-w-full gap-0 px-4 py-1.5">
+        <span class="min-w-20 text-sm font-medium text-highlighted"
+          >Размер:</span
+        >
+
+        <span>{{ species.properties.size }}</span>
+      </div>
+
+      <div class="flex w-full min-w-full gap-0 px-4 py-1.5">
+        <span class="min-w-20 text-sm font-medium text-highlighted"
+          >Скорость:</span
+        >
+
+        <span>{{ species.properties.speed }}</span>
+      </div>
+    </div>
+
     <SpeciesBody
       v-if="species"
       :species
