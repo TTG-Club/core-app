5b862543de50262ed3d24fd00cc1b6a10dd54d8f
Vitalii Ungurean <vitalii@analytics.company>
2025-11-07 09:17:00 +0200

    feat: Снаряжение в бестиарии

--- app/features/bestiary/editor/CreatureEditor.vue
+++ app/features/bestiary/editor/CreatureEditor.vue
@@ -151,6 +151,26 @@
       :proficiency-bonus="state.proficiencyBonus"
     />
 
+    <UCard
+      variant="subtle"
+      class="col-span-full"
+    >
+      <template #header>
+        <h2 class="truncate text-base text-highlighted">Снаряжение</h2>
+      </template>
+
+      <UFormField
+        class="col-span-13"
+        name="ac.text"
+      >
+        <UTextarea
+          v-model="state.equipments"
+          :rows="3"
+          placeholder="Введи снаряжение"
+        />
+      </UFormField>
+    </UCard>
+
     <CreatureDefenses v-model="state.defenses" />
 
     <CreatureSenses
