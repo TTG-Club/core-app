72284ebd0f66e29c9c6c09a0ed5fdce7cf570b56
Vitalii Ungurean <vitalii@analytics.company>
2025-12-24 18:59:12 +0200

    feat: добавлен калькулятор характеристик

--- app/shared/types/character/feats/link.ts
+++ app/shared/types/character/feats/link.ts
@@ -6,3 +6,10 @@ export interface FeatLinkResponse {
   category: string;
   source: SourceResponse;
 }
+
+export interface FeatSelectResponse extends FeatLinkResponse {
+  prerequisite: string;
+  repeatability: boolean;
+  abilities: string[]; // характеристики
+  increase: number; // количество характеристик для увеличения
+}
