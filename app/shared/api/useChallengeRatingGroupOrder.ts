724f04c86cf2aa4b9ae6b23c6b4227ea908b8170
Vitalii Ungurean <vitalii@analytics.company>
2026-01-29 01:11:57 +0200

    hotfix: Исправление сортировки групп в магических предметах

--- app/shared/api/useChallengeRatingGroupOrder.ts
+++ app/shared/api/useChallengeRatingGroupOrder.ts
@@ -1,17 +1,20 @@
 import { computed } from 'vue';
-
 import { DictionaryService } from '~/shared/api/dictionaries';
-import type { ChallengeRatingSelectOption } from '~/shared/types';
+import type { SelectOption } from '~/shared/types';
 
 export function useChallengeRatingGroupOrder() {
-  const { data, pending, error } = useAsyncData<
-    Array<ChallengeRatingSelectOption>
-  >('dictionary-challenge-rating', () => DictionaryService.challengeRating(), {
-    deep: false,
-    default: (): Array<ChallengeRatingSelectOption> => [],
-  });
+  const { data, pending, error } = useAsyncData<Array<SelectOption>>(
+    'dictionary-challenge-rating',
+    async () => {
+      return await DictionaryService.challengeRating();
+    },
+    {
+      deep: false,
+      default: (): Array<SelectOption> => [],
+    },
+  );
 
-  const order = computed(() => {
+  const order = computed<Array<string>>(() => {
     return data.value.map((option) => option.label);
   });
 
