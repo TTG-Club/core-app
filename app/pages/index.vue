f5f812ba8dbfed6c80d33287e170b4eba26f8e4b
peterkogit <31727004+peterkogit@users.noreply.github.com>
2026-02-03 17:38:39 +0300

    feat: добавлена анимация на фоне, на главной (#193)
    * feat: добавлена анимация на фоне, на главной
    
    * refactor: правки главной
    
    * refactor: правки главной
    
    ---------
    
    Co-authored-by: svifty7 <svifty7@gmail.com>

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+  import { HomeBackground } from '~home/background';
   import { HomeBanners, RoadmapBanner } from '~home/banners';
   import { HomeRecentChanges } from '~home/recent-changes';
   import { HomeSections } from '~home/sections';
@@ -19,6 +20,10 @@
 
 <template>
   <NuxtLayout>
+    <ClientOnly>
+      <HomeBackground />
+    </ClientOnly>
+
     <div class="flex flex-col items-center gap-6">
       <h1 class="m-0 h-0 overflow-hidden leading-0 opacity-0">
         {{ name }}
@@ -29,19 +34,19 @@
       <HomeSections />
 
       <div class="flex w-full flex-col gap-3 lg:items-start xl:flex-row">
-        <div class="relative flex w-full flex-col gap-3 xl:w-1/3 2xl:w-1/3">
+        <div class="flex w-full flex-col gap-3 xl:w-1/3 2xl:w-1/3">
           <RoadmapBanner />
 
           <SocialLinks />
         </div>
 
-        <div class="relative flex w-full flex-col gap-3 xl:w-1/3 2xl:w-1/3">
+        <div class="flex w-full flex-col gap-3 xl:w-1/3 2xl:w-1/3">
           <HomeBanners />
 
           <UiPlaceholder class="hidden min-h-32 flex-1 xl:block" />
         </div>
 
-        <div class="relative flex w-full flex-col gap-3 xl:w-1/3 2xl:w-1/3">
+        <div class="flex w-full flex-col gap-3 xl:w-1/3 2xl:w-1/3">
           <HomeRecentChanges />
         </div>
       </div>
