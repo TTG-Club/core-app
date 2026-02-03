3e04c5bef8dcc0e246dbb4df56cbe129287277a3
Peterko <peter.ko@mail.ru>
2026-02-03 19:21:28 +0300

    feat: обновление главной, добавление банера токинатора

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import { HomeBackground } from '~home/background';
-  import { HomeBanners, RoadmapBanner } from '~home/banners';
+  import { HomeBanners, RoadmapBanner, TokenatorBanner } from '~home/banners';
   import { HomeRecentChanges } from '~home/recent-changes';
   import { HomeSections } from '~home/sections';
   import { SocialLinks } from '~home/social-links';
@@ -35,19 +35,21 @@
 
       <div class="flex w-full flex-col gap-3 lg:items-start xl:flex-row">
         <div class="flex w-full flex-col gap-3 xl:w-1/3 2xl:w-1/3">
+          <TokenatorBanner />
+
           <RoadmapBanner />
-
-          <SocialLinks />
-        </div>
-
-        <div class="flex w-full flex-col gap-3 xl:w-1/3 2xl:w-1/3">
-          <HomeBanners />
 
           <UiPlaceholder class="hidden min-h-32 flex-1 xl:block" />
         </div>
 
         <div class="flex w-full flex-col gap-3 xl:w-1/3 2xl:w-1/3">
           <HomeRecentChanges />
+        </div>
+
+        <div class="flex w-full flex-col gap-3 xl:w-1/3 2xl:w-1/3">
+          <HomeBanners />
+
+          <SocialLinks />
         </div>
       </div>
     </div>
