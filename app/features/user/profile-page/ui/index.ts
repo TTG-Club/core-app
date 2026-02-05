559fe799a2ed45a87e2fd47048792fcd22049b9a
Peterko <peter.ko@mail.ru>
2026-02-05 19:24:20 +0300

    feat: обновления UI

--- app/features/user/profile-page/ui/index.ts
+++ app/features/user/profile-page/ui/index.ts
@@ -1,4 +1,5 @@
 export { default as TabConnections } from './TabConnections.vue';
 export { default as TabGeneral } from './TabGeneral.vue';
 export { default as TabSecurity } from './TabSecurity.vue';
+export { default as TabSettings } from './TabSettings.vue';
 export { default as TabStatistics } from './TabStatistics.vue';
