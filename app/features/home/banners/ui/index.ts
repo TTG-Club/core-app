8b332a0dec4659501b2764908d4b8525221ab914
svifty7 <svifty7@gmail.com>
2026-02-12 13:06:58 +0300

    refactor: баннеры на главной

--- app/features/home/banners/ui/index.ts
+++ app/features/home/banners/ui/index.ts
@@ -1,3 +1,1 @@
-export { default as CalculatorAbilitiesBanner } from './CalculatorAbilitiesBanner.vue';
-export { default as RoadmapBanner } from './RoadmapBanner.vue';
-export { default as TokenatorBanner } from './TokenatorBanner.vue';
+export { default as BannerLink } from './BannerLink.vue';
