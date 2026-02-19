b0b620274d6d0d20e90c8716bdbf538c93d3e3f4
Vitaliy Unguryan <gm.magistr@gmail.com>
2026-02-19 11:23:36 +0200

    fix: Добавление разделителя (#206)
    * fix: Добавление разделителя в основные характеристики класса И ИЛИ
    
    * refactor: правки ревью
    
    * refactor: правки ребейза
    
    * refactor: правки ребейза
    
    ---------
    
    Co-authored-by: svifty7 <svifty7@gmail.com>

---
+++ app/features/classes/editor/ui/characteristics/PrimaryCharacteristics.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+  import { SelectAbilities } from '~ui/select';
+
+  import type {
+    AbilityDelimiter,
+    ClassPrimaryCharacteristicsCreate,
+  } from '~classes/model';
+
+  const model = defineModel<ClassPrimaryCharacteristicsCreate>({
+    required: true,
+  });
+
+  const primaryCharacteristicsOperatorOptions: Array<{
+    label: string;
+    value: AbilityDelimiter;
+  }> = [
+    { label: 'И', value: 'AND' },
+    { label: 'ИЛИ', value: 'OR' },
+  ];
+
+  const isDisabled = computed(
+    () => !model.value.values || model.value.values.length <= 1,
+  );
+
+  watch(
+    () => model.value.values,
+    (newValues) => {
+      if (!newValues || newValues.length <= 1) {
+        model.value.delimiter = undefined;
+      }
+    },
+    { deep: true },
+  );
+</script>
+
+<template>
+  <div class="contents">
+    <UFormField
+      class="col-span-10"
+      label="Основная характеристика"
+      name="primaryCharacteristics.values"
+    >
+      <SelectAbilities
+        v-model="model.values"
+        multiple
+      />
+    </UFormField>
+
+    <UFormField
+      class="col-span-4"
+      label="Разделитель"
+      name="primaryCharacteristics.delimiter"
+    >
+      <USelect
+        v-model="model.delimiter"
+        :disabled="isDisabled"
+        :items="primaryCharacteristicsOperatorOptions"
+        placeholder="Разделитель"
+      />
+    </UFormField>
+  </div>
+</template>
