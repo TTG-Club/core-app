6bec403421eb442c5a2327315f74507f55dc0800
Peterko <peter.ko@mail.ru>
2026-04-25 14:59:30 +0300

    feat[vttg]: создание страницы-заглушки для VTTG с центрированным лейаутом

---
+++ app/pages/vttg/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  definePageMeta({
+    layout: 'vttg',
+  });
+
+  useSeoMeta({
+    title: 'VTTG',
+  });
+</script>
+
+<template>
+  <NuxtLayout>
+    <h1
+      class="text-center text-6xl font-bold tracking-tight text-highlighted md:text-8xl"
+    >
+      VTTG
+    </h1>
+  </NuxtLayout>
+</template>
