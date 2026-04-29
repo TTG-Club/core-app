259330a3eb37bbdf3ca87142c425ab054d3a77b3
Vitalii Ungurean <gm.magistr@gmail.com>
2026-04-29 13:31:04 +0300

    feat: Опции для умений класса

---
+++ app/features/classes/body/ui/constants.ts
@@ -0,0 +1,7 @@
+export const FEATURE_OPTIONS_LABELS = {
+  empty: 'Ничего не найдено',
+  levelSuffix: '-й уровень',
+  search: 'Поиск по вариантам...',
+  shown: 'Показано',
+  title: 'Варианты умения',
+} as const;
