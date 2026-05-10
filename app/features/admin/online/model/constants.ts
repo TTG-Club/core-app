64a3740260c1f05c7b6cf03c9b51b31f7a0c3b62
Vitalii Ungurean <gm.magistr@gmail.com>
2026-05-10 14:13:21 +0300

    feat: Отображение пользователей онлайн в панели администрации

---
+++ app/features/admin/online/model/constants.ts
@@ -0,0 +1,32 @@
+export const ADMIN_ONLINE_STATS_API_URL = '/api/online/admin-stats';
+
+export const ADMIN_ONLINE_STATS_DATA_KEY = 'admin-online-stats';
+
+export const ADMIN_ONLINE_STATS_TITLE = 'Пользователи онлайн';
+
+export const ADMIN_ONLINE_STATS_DESCRIPTION =
+  'Статистика активности по сайтам за последнее окно учета.';
+
+export const ADMIN_ONLINE_STATS_WINDOW_LABEL = 'Окно';
+
+export const ADMIN_ONLINE_STATS_MINUTES_LABEL = 'мин';
+
+export const ADMIN_ONLINE_STATS_SITE_LABEL = 'Сайт';
+
+export const ADMIN_ONLINE_STATS_GUESTS_LABEL = 'Гостей';
+
+export const ADMIN_ONLINE_STATS_REGISTERED_LABEL = 'Пользователей';
+
+export const ADMIN_ONLINE_STATS_TOTAL_LABEL = 'Всего';
+
+export const ADMIN_ONLINE_STATS_TOTAL_INLINE_LABEL = 'всего';
+
+export const ADMIN_ONLINE_STATS_SUMMARY_LABEL = 'Все сайты';
+
+export const ADMIN_ONLINE_STATS_REFRESH_LABEL = 'Обновить';
+
+export const ADMIN_ONLINE_STATS_ERROR_TEXT =
+  'Не удалось загрузить статистику онлайн.';
+
+export const ADMIN_ONLINE_STATS_EMPTY_TEXT =
+  'Online-app пока не вернул сайты для отображения.';
