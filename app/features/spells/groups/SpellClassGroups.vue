919256dc0a3294aec20112373563a2b3942cfb29
Vitalii Ungurean <gm.magistr@gmail.com>
2026-07-03 16:12:24 +0300

    feat(spells): добавить автоподгрузку групп по классам

--- app/features/spells/groups/SpellClassGroups.vue
+++ app/features/spells/groups/SpellClassGroups.vue
@@ -1,14 +1,7 @@
 <script setup lang="ts">
   import type { SpellClassPageGroup } from '../model';
 
-  import { PageGrid } from '~ui/page';
-  import { SkeletonLinkSmall } from '~ui/skeleton';
-
-  import { SpellLink } from '../link';
-  import {
-    SPELL_CLASS_GROUP_LOAD_MORE_LABEL,
-    SPELL_CLASS_GROUP_RETRY_LABEL,
-  } from '../model';
+  import SpellClassGroup from './SpellClassGroup.vue';
 
   defineProps<{
     groups: Array<SpellClassPageGroup>;
@@ -26,50 +19,11 @@
 
 <template>
   <div class="flex flex-col gap-6">
-    <section
+    <SpellClassGroup
       v-for="classGroup in groups"
       :key="classGroup.key"
-      class="flex flex-col gap-4"
-    >
-      <USeparator>{{ classGroup.label }}</USeparator>
-
-      <PageGrid
-        v-if="classGroup.spells.length"
-        :columns="3"
-      >
-        <SpellLink
-          v-for="spell in classGroup.spells"
-          :key="spell.url"
-          :spell="spell"
-        />
-      </PageGrid>
-
-      <PageGrid
-        v-if="classGroup.isLoading && !classGroup.spells.length"
-        :columns="3"
-      >
-        <SkeletonLinkSmall
-          v-for="skeletonIndex in 3"
-          :key="skeletonIndex"
-        />
-      </PageGrid>
-
-      <div
-        v-if="classGroup.hasNextPage || classGroup.hasError"
-        class="flex justify-center"
-      >
-        <UButton
-          variant="soft"
-          :loading="classGroup.isLoading"
-          @click.left.exact.prevent="handleLoadMore(classGroup.key)"
-        >
-          {{
-            classGroup.hasError
-              ? SPELL_CLASS_GROUP_RETRY_LABEL
-              : SPELL_CLASS_GROUP_LOAD_MORE_LABEL
-          }}
-        </UButton>
-      </div>
-    </section>
+      :group="classGroup"
+      @load-more="handleLoadMore"
+    />
   </div>
 </template>
